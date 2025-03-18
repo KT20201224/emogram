bad87e6c75b12bf4cd14b263cac01d060f22af4c
KT20201224 <rudxo0525@gmail.com>
2025-03-18 19:50:58 +0900

    Chore: 프로젝트 초기 환경설정 완료
    Docker 설정, Springboot 설정 완료

---
+++ backend/src/test/java/com/emogram/backend/BackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.emogram.backend;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	@Disabled
+	void contextLoads() {
+	}
+
+}

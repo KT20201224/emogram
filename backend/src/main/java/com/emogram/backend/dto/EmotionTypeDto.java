c787147211aa314dab5624bed4a5e3fbdbcfb35e
KT20201224 <rudxo0525@gmail.com>
2025-03-19 17:13:44 +0900

    feat: Emotiontype API,Dto 추가
    EmotionType을 저장하고 조회 가능한 기능 구현

--- backend/src/main/java/com/emogram/backend/dto/EmotionTypeDto.java
+++ backend/src/main/java/com/emogram/backend/dto/EmotionTypeDto.java
@@ -1,2 +1,20 @@
-package com.emogram.backend.dto;public class EmotionTypeDto {
+package com.emogram.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 감정 유형을 전달하는 DTO 클래스
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmotionTypeDto {
+
+    /** 감정 유형 이름 */
+    private String type;
+
+    /** 감정 유형의 색상 코드 */
+    private String color;
 }

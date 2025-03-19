aed64a9695315890e64f2c2c1fe3c24650e34ef2
KT20201224 <rudxo0525@gmail.com>
2025-03-19 16:53:46 +0900

    refactor: EmotionType관련 메서드 추가
    EmotionType 전체 조회, 신규 생성 메서드 추가 및 테스트 코드 추가

--- backend/src/test/java/com/emogram/backend/service/EmotionTypeServiceTest.java
+++ backend/src/test/java/com/emogram/backend/service/EmotionTypeServiceTest.java
@@ -1,5 +1,6 @@
 package com.emogram.backend.service;
 
+import com.emogram.backend.dto.EmotionTypeDto;
 import com.emogram.backend.entity.EmotionType;
 import com.emogram.backend.repository.EmotionTypeRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -60,5 +62,46 @@ class EmotionTypeServiceTest {
         assertEquals("기쁨", foundEmotion.getType());
         assertEquals("#FFD700", foundEmotion.getColor());
         verify(emotionTypeRepository, times(1)).findByType("기쁨");
+    }
+
+    @Test
+    @DisplayName("새로운 감정 유형을 추가")
+    void createEmotionType(){
+        //given
+        EmotionTypeDto requestDto = new EmotionTypeDto("행복", "FFD700");
+        EmotionType savedEntity = new EmotionType("행복", "#FFD700");
+        when(emotionTypeRepository.save(any(EmotionType.class))).thenReturn(savedEntity);
+
+        //when
+        EmotionTypeDto result = emotionTypeService.createEmotionType(requestDto);
+
+        //then
+        assertNotNull(result);
+        assertEquals("행복", result.getType());
+        assertEquals("#FFD700", result.getColor());
+        verify(emotionTypeRepository, times(1)).save(any(EmotionType.class));
+
+    }
+
+    @Test
+    @DisplayName("모든 감정 유형을 조회하는 테스트")
+    void getAllEmotionTypes() {
+        // Given
+        List<EmotionType> emotionTypes = List.of(
+                new EmotionType("기쁨", "#FFFF00"),
+                new EmotionType("슬픔", "#0000FF")
+        );
+
+        when(emotionTypeRepository.findAll()).thenReturn(emotionTypes);
+
+        // When
+        List<EmotionTypeDto> result = emotionTypeService.getAllEmotionTypes();
+
+        // Then
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals("기쁨", result.get(0).getType());
+        assertEquals("슬픔", result.get(1).getType());
+        verify(emotionTypeRepository, times(1)).findAll();
     }
 }

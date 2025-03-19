a13e546bb319ebed6637e4666af178d1d962ac72
KT20201224 <rudxo0525@gmail.com>
2025-03-19 16:20:02 +0900

    feat: 서비스 메서드 작성
    EmotionTypeService 저장하고 type으로 조회하는 메서드 작성

--- backend/src/main/java/com/emogram/backend/service/EmotionTypeService.java
+++ backend/src/main/java/com/emogram/backend/service/EmotionTypeService.java
@@ -1,2 +1,30 @@
-package com.emogram.backend.service;public class EmotionTypeService {
+package com.emogram.backend.service;
+
+import com.emogram.backend.entity.EmotionType;
+import com.emogram.backend.repository.EmotionTypeRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class EmotionTypeService {
+
+    private final EmotionTypeRepository emotionTypeRepository;
+
+    public EmotionTypeService(EmotionTypeRepository emotionTypeRepository){
+        this.emotionTypeRepository = emotionTypeRepository;
+    }
+
+    public EmotionType saveEmotionType(String type, String color){
+        EmotionType emotionType = new EmotionType(type, color);
+
+        return emotionTypeRepository.save(emotionType);
+    }
+
+    public EmotionType getEmotionTypeByName(String type){
+        Optional<EmotionType> emotionType = emotionTypeRepository.findByType(type);
+        return emotionType.orElseThrow(() -> new RuntimeException("해당 감정 유형이 없습니다 : " + type));
+    }
 }

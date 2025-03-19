b5eaebd138dfc95309dd07819f242fdf652b3b2a
KT20201224 <rudxo0525@gmail.com>
2025-03-19 15:44:42 +0900

    feat: Emotiontype 관련 메서드 작성
    EmotionType 엔티티, 레포 구현

--- backend/src/main/java/com/emogram/backend/entity/EmotionType.java
+++ backend/src/main/java/com/emogram/backend/entity/EmotionType.java
@@ -1,2 +1,27 @@
-package com.emogram.backend.entity;public class EmotionType {
+package com.emogram.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "emotion_types")
+public class EmotionType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String type;
+
+    @Column(nullable = false, unique = true)
+    private String color;
+
+    public EmotionType(String type, String color) {
+        this.type = type;
+        this.color = color;
+    }
 }
--- backend/src/main/java/com/emogram/backend/repository/EmotionTypeRepository.java
+++ backend/src/main/java/com/emogram/backend/repository/EmotionTypeRepository.java
@@ -1,2 +1,12 @@
-package com.emogram.backend.repository;public class EmotionTypeRepository {
+package com.emogram.backend.repository;
+
+import com.emogram.backend.entity.EmotionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmotionTypeRepository extends JpaRepository<EmotionType, Long> {
+    Optional<EmotionType> findByType(String type);
 }

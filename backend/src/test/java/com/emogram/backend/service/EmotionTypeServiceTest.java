7901a70061b48300dd32571df1314b7cfc15385f
KT20201224 <rudxo0525@gmail.com>
2025-03-19 15:57:04 +0900

    test: 테스트 코드 작성
    EmotionTypeServiceTest 저장, 조회 기능 테스트 코드 작성

--- backend/src/test/java/com/emogram/backend/service/EmotionTypeServiceTest.java
+++ backend/src/test/java/com/emogram/backend/service/EmotionTypeServiceTest.java
@@ -1,4 +1,62 @@
+package com.emogram.backend.service;
+
+import com.emogram.backend.entity.EmotionType;
+import com.emogram.backend.repository.EmotionTypeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
 class EmotionTypeServiceTest {
-  
+
+    @Mock
+    private EmotionTypeRepository emotionTypeRepository;
+
+    @InjectMocks
+    private EmotionTypeService emotionTypeService;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("감정 유형 저장 테스트")
+    void saveEmotionType(){
+        //given
+        EmotionType emotion = new EmotionType("기쁨", "#FFD700");
+        when(emotionTypeRepository.save(any(EmotionType.class))).thenReturn(emotion);
+
+        //when
+        EmotionType savedEmotion = emotionTypeService.saveEmotionType("기쁨", "FFD700");
+
+        //then
+        assertNotNull(savedEmotion);
+        assertEquals("기쁨", savedEmotion.getType());
+        assertEquals("#FFD700", savedEmotion.getColor());
+        verify(emotionTypeRepository, times(1)).save(any(EmotionType.class));
+    }
+
+    @Test
+    @DisplayName(("감정 유형 조회 테스트"))
+    void findEmotionType(){
+        //given
+        EmotionType emotion = new EmotionType("기쁨", "#FFD700");
+        when(emotionTypeRepository.save(any(EmotionType.class))).thenReturn(emotion);
+
+        //when
+        EmotionType foundEmotion = emotionTypeService.getEmotionTypeByName("기쁨");
+
+        //then
+        assertNotNull(foundEmotion);
+        assertEquals("기쁨", foundEmotion.getType());
+        assertEquals("#FFD700", foundEmotion.getColor());
+        verify(emotionTypeRepository, times(1)).findByType("기쁨");
+    }
 }

ecb830e5a65a30146be9f7079bc3c36abe26ccdf
KT20201224 <rudxo0525@gmail.com>
2025-03-20 16:16:40 +0900

    feat: Controller 메서드 작성
    MemoryOrbController API 구현

--- backend/src/main/java/com/emogram/backend/controller/MemoryOrbController.java
+++ backend/src/main/java/com/emogram/backend/controller/MemoryOrbController.java
@@ -1,2 +1,49 @@
-package com.emogram.backend.controller;public class MemoryOrbController {
+package com.emogram.backend.controller;
+
+import com.emogram.backend.entity.MemoryOrb;
+import com.emogram.backend.service.MemoryOrbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * MemoryOrbController - MemoryOrb 관련 API를 처리하는 컨트롤러 클래스
+ */
+@RestController
+@RequestMapping("/api/memory-orbs")
+public class MemoryOrbController {
+
+    private final MemoryOrbService memoryOrbService;
+
+    @Autowired
+    public MemoryOrbController(MemoryOrbService memoryOrbService) {
+        this.memoryOrbService = memoryOrbService;
+    }
+
+    @PostMapping
+    public MemoryOrb createMemoryOrb(@RequestBody MemoryOrb memoryOrb) {
+        return memoryOrbService.saveMemoryOrb(memoryOrb);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<MemoryOrb> getMemoryOrbById(@PathVariable String id) {
+        return memoryOrbService.getMemoryOrbById(id);
+    }
+
+    @GetMapping
+    public List<MemoryOrb> getAllMemoryOrbs() {
+        return memoryOrbService.getAllMemoryOrbs();
+    }
+
+    @GetMapping("/emotion/{emotionTypeId}")
+    public List<MemoryOrb> getMemoryOrbsByEmotionTypeId(@PathVariable String emotionTypeId) {
+        return memoryOrbService.getMemoryOrbsByEmotionTypeId(emotionTypeId);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMemoryOrb(@PathVariable String id) {
+        memoryOrbService.deleteMemoryOrb(id);
+    }
 }

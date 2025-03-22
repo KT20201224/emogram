d49d64b1bb0aa9ffe868816031a639dfecd2de96
KT20201224 <rudxo0525@gmail.com>
2025-03-22 13:34:05 +0900

    feat: Dto 추가
    EmotionTypeDto 추가

--- backend/src/main/java/com/emogram/backend/dto/EmotionTypeDto.java
+++ backend/src/main/java/com/emogram/backend/dto/EmotionTypeDto.java
@@ -1,2 +1,16 @@
-package com.emogram.backend.dto;public class EmotionTypeDto {
+package com.emogram.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmotionTypeDto {
+
+    private String name;
+    private String color;
 }

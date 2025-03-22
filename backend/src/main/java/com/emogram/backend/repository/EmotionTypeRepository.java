3aac4aa4cc20bdc7cbdfd4405428d0923c60e236
KT20201224 <rudxo0525@gmail.com>
2025-03-22 13:34:38 +0900

    feat: 레포지토리 추가
    EmotionTypeRepository 추가

--- backend/src/main/java/com/emogram/backend/repository/EmotionTypeRepository.java
+++ backend/src/main/java/com/emogram/backend/repository/EmotionTypeRepository.java
@@ -1,2 +1,13 @@
-package com.emogram.backend.repository;public class EmotionTypeRepository {
+package com.emogram.backend.repository;
+
+import com.emogram.backend.entity.EmotionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmotionTypeRepository extends JpaRepository<EmotionType, Long> {
+    Optional<EmotionType> findByName(String name);
+    Optional<EmotionType> findByColor(String color);
 }

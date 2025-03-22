4cd19b48cb3d1344a26fba0ecd4a14e9c5611d45
KT20201224 <rudxo0525@gmail.com>
2025-03-22 13:41:47 +0900

    feat: 컨트롤러 추가
    EmotionTypeController API 구현

--- backend/src/main/java/com/emogram/backend/controller/EmotionTypeController.java
+++ backend/src/main/java/com/emogram/backend/controller/EmotionTypeController.java
@@ -1,2 +1,34 @@
-package com.emogram.backend.controller;public class EmotionTypeController {
+package com.emogram.backend.controller;
+
+import com.emogram.backend.dto.EmotionTypeDto;
+import com.emogram.backend.service.EmotionTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/emotion-types")
+public class EmotionTypeController {
+
+    private final EmotionTypeService emotionTypeService;
+
+    @Autowired
+    public EmotionTypeController(EmotionTypeService emotionTypeService) {
+        this.emotionTypeService = emotionTypeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<EmotionTypeDto> createEmotionType(@RequestBody EmotionTypeDto dto) {
+        EmotionTypeDto createdDto = emotionTypeService.createEmotionType(dto);
+        return new ResponseEntity<>(createdDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EmotionTypeDto>> getAllEmotionTypes() {
+        List<EmotionTypeDto> emotionTypes = emotionTypeService.getAllEmotionTypes();
+        return new ResponseEntity<>(emotionTypes, HttpStatus.OK);
+    }
 }
